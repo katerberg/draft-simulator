87c577ab7e4454e77b18df508ed3bfb36d8f01fb
Mark Katerberg <katerberg@fastmail.fm>
2022-01-04 22:20:54 -0600

    Working draft in roughly real time

--- src/DraftTable.js
+++ src/DraftTable.js
@@ -3,12 +3,37 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function DraftTable({players, picks}) {
+  const [visibleCards, setVisibleCards] = useState(picks.map(row => row.map(cell => '')));
+
+  const makePick = useCallback((pickNumber) => {
+    setVisibleCards(picks.map((row, rowI) => row.map((cell, cellI) => {
+      const cellNumber = rowI % 2 === 0 ? cellI + 1 : row.length - cellI;
+      if (rowI * row.length + cellNumber <= pickNumber) {
+        return picks[rowI][cellI];
+      }
+      return '';
+    })));
+    setTimeout(() => {
+      makePick(pickNumber + 1)
+    }, 30000)
+  }, [picks]);
+
+  useEffect(() => {
+    makePick(1);
+  },[makePick])
+
   return (
       <Table stickyHeader sx={{maxHeight: '440px'}} aria-label="sticky table">
         <TableHead>
           <TableRow>
+            <TableCell
+              key="pick-number"
+            >
+              Pick
+            </TableCell>
             {players.map((player) => (
               <TableCell
                 key={player}
@@ -19,11 +44,14 @@ export default function DraftTable({players, picks}) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {picks
+          {visibleCards
             .map((row, i) => (
               <TableRow hover role="checkbox" tabIndex={-1} key={i}>
-                {row.map((pick) => (
-                  <TableCell key={pick}>
+                <TableCell key={`pick-number${i}`}>
+                    {i+1}
+                </TableCell>
+                {row.map((pick, j) => (
+                  <TableCell key={pick + j}>
                       {pick}
                   </TableCell>
                 ))}
